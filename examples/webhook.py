09be5cc02ba45b96586a8e9247017c9ae4cd6f84
Benjamin Smidt <ben@miruml.com>
2025-08-24 12:38:59 -0700

    chore: refactor examples to be more clear

--- examples/webhook.py
+++ examples/webhook.py
@@ -2,10 +2,18 @@
 import os
 
 # third party imports
+from dotenv import load_dotenv
 from miru_server_sdk import Miru
+
+# set the MIRU_API_KEY environment variable in your '.env' file
+# place this file in the /examples directory
+load_dotenv()
 
 
 def main() -> None:
+    if not os.getenv("MIRU_API_KEY"):
+        raise ValueError("MIRU_API_KEY is not set")
+
     client = Miru(api_key=os.getenv("MIRU_API_KEY"))
 
     event = client.webhooks.unwrap(
